126ec842e2d66aff3b0737f46e57cd45170c8d00
HafeezSalim <59242442+HafeezSalim@users.noreply.github.com>
2025-10-19 11:49:13 +0800

    build function to generate dynamic grid

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-alert("Testing script")
+function generateGrid(size) {
+
+    let grid = document.querySelector(".grid");
+    
+    for(i=1;i<=size;i++) {
+
+        let row = document.createElement("div");
+        row.className = "row";
+        grid.appendChild(row);
+
+        for(j=1;j<=size;j++) {
+
+            let tile = document.createElement("div");
+            tile.className = "tile";
+            row.appendChild(tile);
+
+        }
+    }
+}
+
+generateGrid(16);
